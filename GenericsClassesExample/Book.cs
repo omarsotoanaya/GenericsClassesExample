3fbee2fadc00f74833131d1e2521c74a0a39f9f5
omar soto <alejandro.1915@hotmail.com>
2018-10-09 15:47:32 -0700

    first publish
    first publish

---
+++ GenericsClassesExample/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericsClassesExample
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Autor { get; set; }
+
+    }
+}
--- GenericsClassesExample/Program.cs
+++ GenericsClassesExample/Program.cs
@@ -10,6 +10,23 @@ namespace GenericsClassesExample
     {
         static void Main(string[] args)
         {
+            // Problems that the generic list is the redundant information.
+            // and avoid the penalty performance in the program.
+
+            var book = new Book { Id = 1, Title = "Generic sample", Autor = "Mosh" };
+
+            var books = new BookList();
+            books.Add(book);
+
+            var numbers = new List<int>();
+            numbers.Add(10);
+
+            var genericBookList = new GenericList<Book>();
+            genericBookList.Add(book);
+
+            var genericNumbers = new GenericList<int>();
+            genericNumbers.Add(5);
+                      
         }
     }
 }
